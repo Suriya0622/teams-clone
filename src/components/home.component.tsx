67442ea14322282a17dad638f3cdc6bed8191d5e
Suriya Varman N <suriyavarman.n@tandemloop.in>
2024-12-03 16:24:33 +0530

    Added chat input functionality

--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -1,6 +1,19 @@
-import { ChangeEvent, useRef, useState } from "react";
-import { ChatData } from "../config/chat.config";
+import {
+  ChangeEvent,
+  memo,
+  useContext,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { ChatData, PinnedChatIds } from "../config/chat.config";
 import { loggedInUser } from "../config/user.config";
+import {
+  ChatDataContext,
+  ChatIdContext,
+  UpdateChatDataContext,
+  UpdateChatIdStateContext,
+} from "../context/chat";
 import { Chatmessage, IChatData } from "../models/chat.interface";
 import {
   formatTimestampToMMDDHHMM,
@@ -12,18 +25,29 @@ import {
 import ProfileComponent from "./profile.component";
 
 export function HomePage() {
+  const [chatId, setChatId] = useState<string | null>(ChatData[0].chatId);
+  const [chatData, setChatData] = useState<IChatData[]>(ChatData);
+
   return (
-    <div className="flex grow h-full">
-      {/* Sidebar */}
-      <div className="bg-[#1a1a1a] border-r-[1px] border-[#1f1f1f] w-[360px]">
-        <PeopleHeader />
-        <PeopleList />
-      </div>
-      {/* Chat Section */}
-      <div className="bg-[#1f1f1f] flex flex-col w-full h-full">
-        <ChatContent chat={ChatData[0]} user="Bob" />
-      </div>
-    </div>
+    <ChatDataContext.Provider value={chatData}>
+      <ChatIdContext.Provider value={chatId || ""}>
+        <UpdateChatIdStateContext.Provider value={setChatId}>
+          <UpdateChatDataContext.Provider value={setChatData}>
+            <div className="flex grow h-full">
+              {/* Sidebar */}
+              <div className="bg-[#1a1a1a] border-r-[1px] border-[#1f1f1f] w-[360px]">
+                <PeopleHeader />
+                <PeopleList />
+              </div>
+              {/* Chat Section */}
+              <div className="bg-[#1f1f1f] flex flex-col w-full h-full">
+                <ChatContent />
+              </div>
+            </div>
+          </UpdateChatDataContext.Provider>
+        </UpdateChatIdStateContext.Provider>
+      </ChatIdContext.Provider>
+    </ChatDataContext.Provider>
   );
 }
 
@@ -41,32 +65,21 @@ function PeopleHeader() {
 }
 
 function PeopleList() {
-  let recentChats: string[] = [];
-  let pinnedChats: string[] = [];
-  recentChats = [...ChatData.map((data) => data.chatId)];
-
   return (
     <div className="flex text-[#ffffff] text-[12px] flex-col hover:cursor-pointer select-none">
-      <PeopleDropDown
-        status="Pinned"
-        chat={ChatData.filter((data) => pinnedChats.includes(data.chatId))}
-      />
-      <PeopleDropDown
-        status="Recent"
-        chat={ChatData.filter((data) => recentChats.includes(data.chatId))}
-      />
+      <PeopleDropDown status="Pinned" />
+      <PeopleDropDown status="Recent" />
     </div>
   );
 }
 
-function PeopleDropDown({
-  status,
-  chat,
-}: {
-  status: string;
-  chat: IChatData[];
-}) {
+function PeopleDropDown({ status }: { status: string }) {
+  const chatData = useContext(ChatDataContext);
   const dropDownRef = useRef<HTMLDivElement>(null);
+  const filteredChat =
+    status === "Pinned"
+      ? chatData?.filter((data) => PinnedChatIds.includes(data.chatId))
+      : chatData?.filter((data) => !PinnedChatIds.includes(data.chatId));
 
   function handleDropDownClick() {
     dropDownRef.current?.classList.toggle("hidden");
@@ -82,26 +95,21 @@ function PeopleDropDown({
         <p>{status}</p>
       </div>
       <div className="flex flex-col" ref={dropDownRef}>
-        {chat.map((data) => (
-          <PeopleCard
-            userName={data.user1 === loggedInUser ? data.user2 : data.user1}
-            recentMessage={sortMessage(data.messages)}
-          />
+        {filteredChat?.map((data) => (
+          <PeopleCard chatData={data} key={data.chatId} />
         ))}
       </div>
     </>
   );
 }
 
-function PeopleCard({
-  userName,
-  recentMessage,
-}: {
-  userName: string;
-  recentMessage: Chatmessage[] | null;
-}) {
+function PeopleCard({ chatData }: { chatData: IChatData }) {
   const cardHover = useRef<HTMLDivElement>(null);
+  const updateActiveId = useContext(UpdateChatIdStateContext);
   const [showThreeDot, setShowThreeDot] = useState(false);
+  const cardUser =
+    chatData.user1 === loggedInUser ? chatData.user2 : chatData.user1;
+  const sortedMessage = sortMessage(chatData.messages);
 
   function handleCardHoverStart() {
     cardHover.current?.classList.toggle("text-[#ffffff]");
@@ -117,20 +125,27 @@ function PeopleCard({
       className="flex items-center h-[49px] pl-[15px] ml-[5px] mr-[20px] gap-[6px] hover:border-[0.5px] hover:h-[48px] hover:rounded-md hover:border-[#525252] hover:mr-[18px]"
       onMouseEnter={handleCardHoverStart}
       onMouseLeave={handleCardHoverEnd}
+      onClick={() => {
+        if (updateActiveId) {
+          updateActiveId((id) => chatData.chatId);
+        }
+      }}
     >
-      <ProfileComponent userName={getShortName(userName)} />
+      <ProfileComponent userName={getShortName(cardUser)} />
       <div className="grid grid-flow-col">
         <div className="min-w-[240px]">
-          <p className="text-[14px] font-normal">{userName}</p>
+          <p className="text-[14px] font-normal">{cardUser}</p>
           <p
             className="text-[12px] text-[#adadad] truncate max-w-[240px] overflow-hidden whitespace-nowrap"
             ref={cardHover}
           >
-            {recentMessage?.[0]
-              ? `${
-                  userName === recentMessage[0].senderName ? userName : "You"
-                }: ${recentMessage[0].content}`
-              : "No recent messages"}
+            {`${
+              sortedMessage
+                ? sortedMessage[0].senderName !== cardUser
+                  ? "You: " + sortedMessage[0].content
+                  : sortedMessage[0].content
+                : ""
+            }`}
           </p>
         </div>
         <p
@@ -140,7 +155,7 @@ function PeopleCard({
         >
           {getTimeStampInMMDD(
             formatTimestampToMMDDHHMM(
-              recentMessage?.[0] ? recentMessage[0]?.timestamp : null
+              sortedMessage ? sortedMessage[0].timestamp : null
             )
           )}
         </p>
@@ -156,11 +171,18 @@ function PeopleCard({
   );
 }
 
-function ChatHeader({ chat, user }: { chat: IChatData; user: string }) {
+function ChatHeader() {
+  const activeChatId = useContext(ChatIdContext);
+  const chatData = useContext(ChatDataContext);
+  const activeChatData = chatData?.find((chat) => chat.chatId === activeChatId);
+  const user =
+    activeChatData?.user1 === loggedInUser
+      ? activeChatData.user2
+      : activeChatData?.user1;
   return (
     <div className="flex h-[61px] border-b-[1px] border-[#101010]">
       <div className="flex flex-auto items-center justify-start gap-[15px] pl-[15px] basis-[20%]">
-        <ProfileComponent userName={getShortName(user)} />
+        <ProfileComponent userName={getShortName(user || "")} />
         <p className="text-[18px] font-bold text-[#ffffff]">{user}</p>
 
         <div className="flex items-end gap-[20px] text-[14px] h-[61px] text-[#9ca3af]">
@@ -189,44 +211,47 @@ function ChatHeader({ chat, user }: { chat: IChatData; user: string }) {
   );
 }
 
-function ChatContent({ chat, user }: { chat: IChatData; user: string }) {
+function ChatContent() {
   return (
     <>
-      <ChatHeader chat={chat} user={user} />
-      <ChatBox chat={chat} />
+      <ChatHeader />
+      <ChatBox />
     </>
   );
 }
 
-function ChatBox({ chat }: { chat: IChatData }) {
+function ChatBox() {
   return (
     <div className="flex flex-col h-full mx-[15%] mb-[2%]">
-      <ChatMessage chat={chat} />
+      <ChatMessage />
       <ChatInput />
     </div>
   );
 }
 
-function ChatMessage({ chat }: { chat: IChatData }) {
-  let sortedMessages = sortMessage(chat.messages)?.reverse();
+const ChatMessage = memo(function ChatMessage() {
+  const activeChatId = useContext(ChatIdContext);
+  const chatData = useContext(ChatDataContext);
+  const activeChatData = chatData?.find((chat) => chat.chatId === activeChatId);
+  let sortedMessages = sortMessage(activeChatData?.messages || [])?.reverse();
   return (
-    <div className="bg-red-100 flex-grow w-auto overflow-auto mb-[2%]">
-      {sortedMessages?.map((message) => {
+    <div className="flex-grow w-auto overflow-auto mb-[2%]">
+      {sortedMessages?.map((message, index) => {
         if (isLoggedInUser(message.senderName)) {
-          return <ChatRightMessage message={message} />;
+          return <ChatRightMessage message={message} key={index} />;
         } else {
-          return <ChatLeftMessage message={message} />;
+          return <ChatLeftMessage message={message} key={index} />;
         }
       })}
     </div>
   );
-}
+});
 
 function ChatLeftMessage({ message }: { message: Chatmessage }) {
   return (
-    <div className="flex justify-start flex-col">
-      <ProfileComponent userName={message.senderName} />
-      <div className="flex flex-col max-w-[80%] gap-[5px]">
+    <div className="flex justify-start gap-[5px]">
+      <ProfileComponent userName={getShortName(message.senderName)} />
+      <div className="flex flex-col max-w-[80%]">
         <p className="flex items-center text-[12px] text-[#adadad]">{`${
           message.senderName
         } ${formatTimestampToMMDDHHMM(message.timestamp)}`}</p>
@@ -239,28 +264,81 @@ function ChatLeftMessage({ message }: { message: Chatmessage }) {
 }
 
 function ChatRightMessage({ message }: { message: Chatmessage }) {
-  return <div className="flex justify-end">{message.content}</div>;
+  return (
+    <div className="flex justify-end">
+      <div className="flex flex-col">
+        <p className="flex items-center justify-end text-[12px] text-[#adadad]">
+          {formatTimestampToMMDDHHMM(message.timestamp)}
+        </p>
+        <p className="h-[36px] bg-[#2b2b40] rounded text-[14px] text-[#ffffff] flex items-center px-[10px]">
+          {message.content}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 function ChatInput() {
+  const [inputMessage, setInputMessage] = useState<string>("");
+  const activeChatId = useContext(ChatIdContext);
+  const chatData = useContext(ChatDataContext);
+  const setChatData = useContext(UpdateChatDataContext);
   const [height, setHeight] = useState("44px"); // Initial height
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const activeChatData = useMemo(
+    () => chatData?.find((chat) => chat.chatId === activeChatId),
+    []
+  );
 
   const autoGrow = (event: ChangeEvent<HTMLTextAreaElement>) => {
     const element = event.target;
     // Reset height to calculate scrollHeight accurately
     element.style.height = "44px";
     element.style.height = `${element.scrollHeight}px`; // Adjust height based on scrollHeight
-    console.log(element.scrollHeight);
     textAreaRef.current?.setAttribute("height", element.style.height);
     setHeight(element.style.height);
   };
+
+  function handleInputmessage(event: ChangeEvent<HTMLTextAreaElement>): void {
+    setInputMessage(event.target.value);
+  }
+
+  function handleSendMessage() {
+    if (inputMessage) {
+      const newMessage: Chatmessage = {
+        senderId:
+          loggedInUser === activeChatData?.user1
+            ? activeChatData?.user2
+            : activeChatData?.user1 || "",
+        senderName: loggedInUser,
+        content: inputMessage,
+        timestamp: new Date(),
+      };
+      setInputMessage((message) => "");
+      textAreaRef.current?.setAttribute("height", "44px");
+      setHeight((h) => "44px");
+
+      if (setChatData) {
+        setChatData((data) =>
+          data.map(
+            (chat) =>
+              chat.chatId === activeChatId
+                ? { ...chat, messages: [...chat.messages, newMessage] }
+                : chat // Return the unchanged chat if condition is not met
+          )
+        );
+      }
+    }
+  }
   return (
     <div className="w-[85%] flex items-center bg-[#292929] !border !border-[#525252] rounded-lg px-3 inline-block mx-auto">
       <textarea
         ref={textAreaRef}
+        value={inputMessage}
         placeholder="Type a message"
         onInput={autoGrow}
+        onChange={handleInputmessage}
         className={`h-[${height}] py-[11px] resize-none overflow-hidden min-h-[40px] max-h-[100px] bg-inherit flex-1 bg-transparent text-sm text-gray-300 placeholder-gray-500 placeholder:text-sm px-3 focus:outline-none`}
       ></textarea>
       <div className="flex gap-[15px] text-gray-500">
@@ -269,13 +347,22 @@ function ChatInput() {
           <ChartHeaderIcons iconName="bi-emoji-smile-fill" />
           <ChartHeaderIcons iconName="bi-plus-lg" />
         </div>
-        <ChartHeaderIcons iconName="bi-send-fill rotate-45" />
+        <ChartHeaderIcons
+          iconName="bi-send-fill rotate-45"
+          clickEvent={handleSendMessage}
+        />
       </div>
     </div>
   );
 }
 
-function ChartHeaderIcons({ iconName }: { iconName: string }) {
+function ChartHeaderIcons({
+  iconName,
+  clickEvent,
+}: {
+  iconName: string;
+  clickEvent?: () => void;
+}) {
   const [hoverActive, setHoverActive] = useState(false);
 
   function handleHover() {
@@ -294,6 +381,7 @@ function ChartHeaderIcons({ iconName }: { iconName: string }) {
       }`}
       onMouseEnter={handleHover}
       onMouseLeave={handleHoverEnd}
+      onClick={clickEvent}
     ></i>
   );
 }
